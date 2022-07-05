a1d4ff57e27d897bba923e2d840263e1ae105713
vmichailcc <vmichail2022@gmail.com>
2022-07-05 10:15:05 +0300

    start DRF

--- drf/store/views.py
+++ drf/store/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(http_method_names=['GET'])
+def hello_world(request):
+    return_dict = {"message": "Hello, world!"}
+    return Response(return_dict)
